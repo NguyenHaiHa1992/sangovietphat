6999311be8f030642c3a5b6f9f463b5129b13e2d
HOANGTUNG\T430 <tunghus1993@gmail.com>
2017-06-26 22:45:02 +0700

    change config main

--- protected/config/main.php
+++ protected/config/main.php
@@ -116,6 +116,10 @@ return array(
 					'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
 				),
+//                                array(
+//                                    'class'=>'ext.yii-debug-toolbar.YiiDebugToolbarRoute',
+//                                    'ipFilters'=>array('127.0.0.1','192.168.1.215'),
+//                                ),
 				// uncomment the following to show log messages on web pages
 				/*
 				array(
