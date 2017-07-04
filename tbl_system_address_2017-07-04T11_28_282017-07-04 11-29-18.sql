2be0582b182bfd359e31f7107d93a00e8d669c3e
tunglexuan <tunglx@rikkeisoft.com>
2017-07-04 11:32:12 +0700

    add table system_store and system_address mysql

---
+++ tbl_system_address_2017-07-04T11_28_282017-07-04 11-29-18.sql
@@ -0,0 +1,17 @@
+SELECT * FROM sangovietphat.tbl_system_address
+
+INSERT INTO `sangovietphat`.`tbl_system_address`(`id`,`parent_id`,`name`,`content`,`status`,`order_view`,`created_time`,`created_by`) VALUES (1,1,'Tại Hà Nội','Địa chỉ 1: Số 31 Ngọc Đại , Phường Đại Mỗ ,
+
+Quận Nam Từ Liêm , Hà Nội
+
+Điện thoại : 043.200.2097 - 0982 088 969',1,0,null,null);
+INSERT INTO `sangovietphat`.`tbl_system_address`(`id`,`parent_id`,`name`,`content`,`status`,`order_view`,`created_time`,`created_by`) VALUES (2,2,'Hải phòng','Đia chỉ 1: 80 Nguyễn Bỉnh Khiêm - Quận Ngô Quyền,
+
+Hải phòng
+
+Điện thoại : 0313.734.270 - 0904.334.667',1,1,null,null);
+INSERT INTO `sangovietphat`.`tbl_system_address`(`id`,`parent_id`,`name`,`content`,`status`,`order_view`,`created_time`,`created_by`) VALUES (3,2,'Hải phòng','Địa chỉ 2: Số 10 Nguyễn Văn Linh - Quận Lê Chân -
+
+Hải Phòng
+
+Điện thoại : 0313.622.928 - 0934.316.230',1,2,null,null);
---
+++ tbl_system_store_2017-07-04T11_30_232017-07-04 11-30-50.sql
@@ -0,0 +1,4 @@
+SELECT * FROM sangovietphat.tbl_system_store
+
+INSERT INTO `sangovietphat`.`tbl_system_store`(`id`,`name`,`order_view`,`status`,`created_by`,`created_time`) VALUES (1,'Hà Nội',0,1,null,null);
+INSERT INTO `sangovietphat`.`tbl_system_store`(`id`,`name`,`order_view`,`status`,`created_by`,`created_time`) VALUES (2,'Hải Phòng',1,1,null,null);
