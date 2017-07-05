6ba74e71c29ad65376328aa7ae82b513bdd7f05e
HOANGTUNG <tunghus1993@gmail.com>
2017-07-05 23:23:05 +0700

    update footer

--- protected/components/iphoenix_widget/views/wFooterLink.php
+++ protected/components/iphoenix_widget/views/wFooterLink.php
@@ -1,35 +1,37 @@
 <!-- wFooterLink -->
+
+<?php 
+$count = count($data);
+$row = 6;
+$datas = array_chunk($data, $row);
+
+?>
 <div id="<?php echo $id;?>" class="<?php echo $class;?>">
         <div class="inside space-padding-tb-40">
-            <div class="row">
-                <div class="col-md-6 col-sm-6 col-xs-12 column">
-                    <div class="panel">
-                        <div class="panel-body">
-                            
-                          <ul class="list-unstyled">
-                              <?php foreach ($data as $id=>$item) {
-                                $menu = $item['content'];
-                                if(isset($menu->other['url'])){
-                                    echo '<li><a id="'.$id. '" href="'.$menu->url.'" target='.$menu->target.'>'.$menu->name.'</a></li>';
-                                }
-                            }?>
-                          </ul>
-                            
-                        </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 border-line bg-blur">
+                <div class="widget-html">
+                    <div class="widget-inner">
+                            <?php foreach($datas as $key => $data) :?>
+                            <div class="col-md-3 col-sm-6 col-xs-12 column">
+                                <div class="panel none-shadow">
+                                    <div class="panel-body">
+                                      <ul class="list-unstyled">
+                                          <?php foreach ($data as $id=>$item) {
+                                            $menu = $item['content'];
+                                            if(isset($menu->other['url'])){
+                                                echo '<li><a id="'.$id. '" href="'.$menu->url.'" target='.$menu->target.'>'.$menu->name.'</a></li>';
+                                            }
+                                        }?>
+                                      </ul>
+                                    </div>
+                                </div>
+                              </div>
+                              <?php endforeach;?>
+
+                            <div class="col-xs-12 col-sm-6 col-md-4 pull-right">
+                                <iframe width="100%" height="220" src="https://www.google.com/maps/d/embed?mid=1xHioeCHPf5Iaj6FQeLRtVjrfeX0"></iframe>
+                            </div>
                     </div>
-                  </div>
-                
-                <div class="col-xs-12 col-sm-6 col-md-6 column">
-                    <div class="custom-logo" style="line-height: 1.4;">
-                        <address class="space-top-10">
-                            <span style="font-weight: bold; text-transform: uppercase; line-height: 1.42857;">
-                                HỆ THỐNG BÁN LẺ VẬT LIỆU HOÀN THIỆN TẠI KHO<br>
-                            </span>
-                            <span style="font-weight: bold;">HOTLINE</span>
-                            : <a href="tel:18000022">1800 0022</a>
-                        </address>
-                    </div>
-                    <iframe width="100%" height="220" src="https://www.google.com/maps/d/embed?mid=1xHioeCHPf5Iaj6FQeLRtVjrfeX0"></iframe>
                 </div>
             </div>
         </div>
