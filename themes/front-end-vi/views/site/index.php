c5b1e704b6ab79182da6c783747cf929ca1353e6
HOANGTUNG <tunghus1993@gmail.com>
2017-07-17 23:14:31 +0700

    tang so luong category trang chu

--- themes/front-end-vi/views/site/index.php
+++ themes/front-end-vi/views/site/index.php
@@ -7,7 +7,7 @@
         });
     });
 </script>-->
-<?php $list_category = ProductCategory::getItems(5, array('parent_id' => 0,)); ?>
+<?php $list_category = ProductCategory::getItems(10, array('parent_id' => 0,)); ?>
 <div class="col-md-12" id="main-content">
     <div class="col-sm-12">
         <!--<div id="slider" class="carousel slide wContentBox" data-ride="carousel">-->
